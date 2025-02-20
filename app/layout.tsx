c2aae1dc4464602a091a5e3ca339574ee762f12b
LurchingDart <lurchdev@gmail.com>
2025-02-20 15:20:14 -0600

    ADD: Integrate Vercel Analytics into the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,24 @@
 import type { Metadata } from "next";
 import { DynaPuff, Cherry_Bomb_One } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const dynaPuff = DynaPuff({
-    weight: "400",
-    variable: "--font-dyna",
-    subsets: ["latin"],
+  weight: "400",
+  variable: "--font-dyna",
+  subsets: ["latin"],
 });
 
 const cherryBombOne = Cherry_Bomb_One({
-    weight: "400",
-    variable: "--font-cherry-bomb-one",
-    subsets: ["latin"],
+  weight: "400",
+  variable: "--font-cherry-bomb-one",
+  subsets: ["latin"],
 });
 
 
 export const metadata: Metadata = {
-    title: "Happy Happy CUM!",
-    description: "Happy Cum para Bel, la mas linda de todas",
+  title: "Happy Happy CUM!",
+  description: "Happy Cum para Bel, la mas linda de todas",
 };
 
 export default function RootLayout({
@@ -31,6 +32,7 @@ export default function RootLayout({
         className={`${dynaPuff.variable} ${cherryBombOne.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
